db2f3ec22ee124d7b3a42cb66a2baea3018d7918
Matheus Silva <matheus_lsilva@hotmail.com>
2022-09-22 16:17:29 -0300

    fix: retornando dicionario apos ser criado

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.json_handle import read_json, write_json
 DATA_BASE_PATCH = "./menu.json"
 
 if __name__ == "__main__":
-    # print(read_json(DATA_BASE_PATCH))
+    print(read_json(DATA_BASE_PATCH))
 
-    new_item = {"name": "CHURROS DO M5", "price": 5.0}
-    write_json(DATA_BASE_PATCH, new_item)
+    # new_item = {"name": "CHURROS DO M5", "price": 5.0}
+    # print(write_json(DATA_BASE_PATCH, new_item))
--- utils/json_handle.py
+++ utils/json_handle.py
@@ -1,5 +1,6 @@
 import json
 
+
 def next_id(database_patch: str) -> int:
     products = read_json(database_patch)
 
@@ -7,6 +8,7 @@ def next_id(database_patch: str) -> int:
         return 1
 
     return products[-1]['id'] + 1
+
 
 def read_json(database_patch: str) -> list:
     try:
@@ -18,19 +20,21 @@ def read_json(database_patch: str) -> list:
         return []
 
 
-def write_json(database_patch: str, item: dict) -> None:
+def write_json(database_patch: str, item: dict) -> dict:
 
     database_data = read_json(database_patch)
 
     item.update({"id": next_id(database_patch)})
-    
+
     organized_item = {
-      "id": item["id"],
-      "name": item["name"],
-      "price": item["price"]
+        "id": item["id"],
+        "name": item["name"],
+        "price": item["price"]
     }
 
     database_data.append(organized_item)
 
     with open(database_patch, "w") as file:
         json.dump(database_data, file, indent=4)
+
+    return organized_item
